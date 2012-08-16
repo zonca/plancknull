18a1904abbac0499eb70454da180a109ef410cb8
Andrea Zonca <code@andreazonca.com>
2012-08-16 01:52:20 -0700

    added nside, tested on DX9, untested subsets

--- reader.py
+++ reader.py
@@ -14,17 +14,19 @@ class BaseMapReader:
     """Abstract class, all readers should provide this
     interface"""
 
-    def __call__(self, freq, surv, chtag='', halfring=0, pol='I'):
+    def __call__(self, freq, surv, nside=None, chtag='', halfring=0, pol='I'):
         """Read a map and return the array of pixels.
         
         Parameters
         ----------
         freq : int
             frequency
-        chtag : string 
-            can be "" for frequency, radiometer("LFI18S"), horn("LFI18"), quadruplet("LFI18-LFI23"), detset("detset_1")
         surv : int or string
             "nominal", "full", or survey number
+        nside : None or int
+            if None matches any nside, otherwise integer nside
+        chtag : string 
+            can be "" for frequency, radiometer("LFI18S"), horn("LFI18"), quadruplet("LFI18-LFI23"), detset("detset_1")
 
         Returns
         -------
@@ -40,7 +42,7 @@ class SingleFolderDXReader:
     def __init__(self, folder):
         self.folder = folder
 
-    def __call__(self, freq, surv, chtag='', halfring=0, pol="I"):
+    def __call__(self, freq, surv, nside=None, chtag='', halfring=0, pol="I"):
         # stokes component
         components = [stokes.index(p) for p in pol]
         if len(components) == 1:
@@ -59,7 +61,25 @@ class SingleFolderDXReader:
         if isinstance(surv, int):
             surv = "survey_%d" % surv
 
+        # nside
+        if nside:
+            nside_tag = "%d" % nside
+        else:
+            nside_tag = "????"
+
         # read_map
-        filename = glob(os.path.join(self.folder, "???_%d_????_????????%s_%s.fits*" % (freq, subset_halfring_tag, surv)))
-        log.info("Reading %s" % filename)
-        return hp.read_map(filename, components)
+        filename_pattern = os.path.join(self.folder, "???_%d_%s_????????%s_%s.fits*" % (freq, nside_tag, subset_halfring_tag, surv))
+        filename = glob(filename_pattern)
+        if len(filename) == 1:
+            filename = filename[0]
+        else: 
+            if len(filename) == 0:
+                error_log = "No match for pattern " + filename_pattern
+            else:
+                error_log = "Multiple matches for pattern " + filename_pattern
+            log.fatal(error_log)
+            raise exceptions.IOError(error_log)
+
+        log.info("Reading %s" % os.path.basename(filename))
+        #return hp.read_map(filename, components)
+        return None

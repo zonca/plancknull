c208a99aaa12bb02bf44b4bd0323452e3af1965c
Andrea Zonca <code@andreazonca.com>
2012-09-25 16:59:42 -0700

    fixed bug in smoothing algorithm

--- utils.py
+++ utils.py
@@ -1,7 +1,10 @@
 import numpy as np
 import healpy as hp
 
-def smooth_variance_map(var_m, orig_fwhm, fwhm):
+def get_chisq(m, var):
+    return np.mean(m**2/var)
+
+def smooth_variance_map(var_m, fwhm):
     """Smooth a variance map
 
     Algorithm from 'Pixel errors in convolved maps'
@@ -11,8 +14,6 @@ def smooth_variance_map(var_m, orig_fwhm, fwhm):
     ----------
     var_m : array
         input variance map
-    orig_fwhm : float (radians)
-        original fwhm
     fwhm : float (radians)
         target fwhm
 
@@ -28,7 +29,7 @@ def smooth_variance_map(var_m, orig_fwhm, fwhm):
 
     # normalization factor
     pix_area = hp.nside2pixarea(hp.npix2nside(len(var_m)))
-    orig_beam_width = orig_fwhm/np.sqrt(8*np.log(2))
+    orig_beam_width = fwhm/np.sqrt(8*np.log(2))
     A_vb = pix_area / (4. * np.pi * orig_beam_width**2)
     smoothed_var_m *= A_vb
 
